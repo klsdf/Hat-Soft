dc5f64d7358a9d98fd5a93856cf09e3e485d9708
ycx <1796655849@qq.com>
2024-01-18 16:13:59 +0800

    修改端口

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
-var serverUrl = `http://106.14.74.242:3000`
+// var serverUrl = `http://106.14.74.242:3000`
+var serverUrl = `https://106.14.74.242:443`
+
 // var serverUrl = `http://localhost:3000`
